94fbd9b8a74fcf6702ff3af7ba5794cf794c1836
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-31 13:16:48 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    class App extends Component {
+      render() {
+        return (
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Complete Nebullam Code Challenge</h5>
+              <h6 class="card-subtitle mb-2 text-muted">This must be completed by Monday evening if you are going to get everything else done this week.</h6>
+              <p class="card-text">You will most likely not be needing this section, but let's keep it just in case for now. :-)</p>
+            </div>
+          </div>
+        );
+      }
+    }
 
-export default App;
+    export default App;
