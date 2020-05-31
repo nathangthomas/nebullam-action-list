ec3df83da3272677c4c8a6d53f4bc68afbd6be74
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-31 17:48:17 -0600

    Fetch all tasks and begin to create form to add tasks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Tasks from './components/tasks';
+// import NewTaskForm from './components/newTaskForm'
 
-    class App extends Component {
-      render() {
+class App extends Component {
+    render() {
         return (
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Complete Nebullam Code Challenge</h5>
-              <h6 class="card-subtitle mb-2 text-muted">This must be completed by Monday evening if you are going to get everything else done this week.</h6>
-              <p class="card-text">You will most likely not be needing this section, but let's keep it just in case for now. :-)</p>
-            </div>
-          </div>
-        );
-      }
+            <Tasks tasks={this.state.tasks} />
+            // <NewTaskForm/>
+        )
     }
 
-    export default App;
+    state = {
+        tasks: []
+    };
+
+    componentDidMount() {
+        fetch('http://nathan.interview.nebullam.com:1880/tasks')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ tasks: data })
+            })
+            .catch(console.log)
+    }
+}
+
+export default App;
