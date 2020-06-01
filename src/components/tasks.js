618b40b3809f5d6d3512b5bdfd72cbb58a679166
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-31 22:10:58 -0600

    Bold task names

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -28,48 +28,63 @@ export class Tasks extends Component {
     this.refreshList();
   }
 
+  deleteTask(id){
+    if(window.confirm('Are you sure?')){
+      fetch('http://nathan.interview.nebullam.com:1880/tasks/'+id, {
+        method:'DELETE',
+        headers:{'Accept':'application/json', 'Content-Type':'application/json'}
+      })
+    }
+  }
+
   render(){
   const {tasks, id, name, details, complete} = this.state;
   let addModalClose=() => this.setState({addModalShow:false});
-  let editModalClose=() => this.setState({editModalShow:false});
+  let editModalClose=() =>  this.setState({editModalShow:false});
 
     return(
       <div>
-        <Table className="mt-4" stripped bordered hover>
+        <Table className="mt-4" stripped bordered hover size="sm">
         <thread>
-          <tr>
-            <th> Action Items </th>
-            <th> Details </th>
-            <th> Complete </th>
-            <th> Options </th>
+        <tr>
+          <th><h3>Action Items</h3></th>
+          <th><h3>Details</h3></th>
+          <th><h3>Complete</h3></th>
+          <th><h3>Options</h3></th>
+        </tr>
 
-          </tr>
         </thread>
         <tbody>
           {tasks.map(task=>
             <tr key ={task.id}>
-              <td>{task.name}</td>
-              <td>{task.details}</td>
-              <td>{task.complete}</td>
-              <td>
-                <ButtonToolbar>
-                  <Button
-                    className="mr-2"
-                    variant="info"
-                    onClick = {()=> this.setState({editModalShow:true, id:task.id, name:task.name, details:task.details, complete: task.complete})}>
-                      Edit
-                  </Button>
+            <td><strong>{task.name}</strong></td>
+            <td>{task.details}</td>
+            <td>{task.complete}</td>
+            <td>
+              <ButtonToolbar>
+                <Button
+                  className="mr-2"
+                  variant="info"
+                  onClick = {()=> this.setState({editModalShow:true, id:task.id, name:task.name, details:task.details, complete: task.complete})}>
+                    Edit
+                </Button>
 
-                  <EditTaskModal
-                  show={this.state.editModalShow}
-                  onHide={editModalClose}
-                  id = {this.state.id}
-                  name = {this.state.name}
-                  details = {this.state.details}
-                  />
+                <Button className="mr-2"
+                  onClick= {()=> this.deleteTask(task.id)}
+                  variant="danger">
+                    Delete
+                </Button>
 
-                </ButtonToolbar>
-              </td>
+                <EditTaskModal
+                show={this.state.editModalShow}
+                onHide={editModalClose}
+                id = {this.state.id}
+                name = {this.state.name}
+                details = {this.state.details}
+                />
+
+              </ButtonToolbar>
+            </td>
             </tr>
           )}
         </tbody>
