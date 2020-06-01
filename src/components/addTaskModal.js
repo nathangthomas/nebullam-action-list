0200d85b6ce10e2ebdb1c63211d4f9aab38b8215
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-31 18:48:50 -0600

    Add material-ui snackbar

--- src/components/addTaskModal.js
+++ src/components/addTaskModal.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import{Modal, Button, Row, Col, Form} from 'react-bootstrap'
-// import NewTaskForm from './newTaskForm'
+import Snackbar from '@material-ui/core/Snackbar'
+import IconButton from '@material-ui/core/IconButton'
 
 
 export class AddTaskModal extends Component {
 
   constructor(props){
     super(props);
+
+    this.state = {snackbaropen: false, snackbarmsg: ''}
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  snackbarClose = (event) =>{
+    this.setState({snackbaropen: false});
+  };
 
   handleSubmit(event){
     event.preventDefault();
@@ -26,53 +34,66 @@ export class AddTaskModal extends Component {
     })
     .then(res=> res.json())
     .then((result)=> {
-      alert("Your Action Item has been added!")
+      this.setState({snackbaropen:true, snackbarmsg:"Your Action Item has been added! "})
     },
     (error)=>{
-      alert('Failed')
+      this.setState({snackbaropen:true, snackbarmsg:'Failed'})
     })
   }
 
   render(){
     return(
-      <Modal
-      {...this.props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add A New Task
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="container">
-          <Row>
-            <Col sm={6}>
-              <Form onSubmit={this.handleSubmit}>
-                <Form.Group controlId="name">
-                  <Form.Label> Action Item </Form.Label>
-                  <Form.Control type="text" name="name" required placeholder="Action Item"/>
-                </Form.Group>
+      <div className="container">
 
-                <Form.Group controlId="details">
-                  <Form.Label> Description </Form.Label>
-                  <Form.Control type="text" name="details" required placeholder="Description"/>
-                </Form.Group>
+        <Snackbar
+          anchorOrigin={{vertical:'bottom', horizontal:'center'}}
+          open = {this.state.snackbaropen} autoHideDuration = {3000} onClose={this.snackbarClose}
 
-                <Form.Group>
-                <Button variant="primary" type="submit"> Add Action Item </Button>
-                </Form.Group>
-              </Form>
-            </Col>
-          </Row>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button varient="danger" onClick={this.props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+          message = {<span id="message-id">{this.state.snackbarmsg}</span>}
+          action={[
+            <IconButton
+              key="close"
+              arial-label="Close"
+              color="inherit"
+              onClick={this.snackbarClose}
+              >
+                x
+            </IconButton>
+          ]}
+          />
+
+        <Modal{...this.props} size="lg" aria-labelledby="contained-modal title-vcenter" centered>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add A New Task
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Row>
+              <Col sm={6}>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="name">
+                    <Form.Label> Action Item </Form.Label>
+                    <Form.Control type="text" name="name" required placeholder="Action Item"/>
+                  </Form.Group>
+
+                  <Form.Group controlId="details">
+                    <Form.Label> Description </Form.Label>
+                    <Form.Control type="text" name="details" required placeholder="Description"/>
+                  </Form.Group>
+
+                  <Form.Group>
+                  <Button variant="primary" type="submit"> Add Action Item </Button>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button varient="danger" onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
     );
   }
 }
