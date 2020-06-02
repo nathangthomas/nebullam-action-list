88a44a86d5e8e3db090dcfd803d8b67d357ed36c
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-06-01 22:30:05 -0600

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React, {Component} from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Tasks from './components/tasks';
-// import NewTaskForm from './components/newTaskForm'
 
-class App extends Component {
-    render() {
-        return (
-            <Tasks tasks={this.state.tasks} />
-        )
-    }
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/user_tasks/0">Clayton</Link>
+            </li>
+            <li>
+              <Link to="/user_tasks/1">Josh</Link>
+            </li>
+          </ul>
+        </nav>
 
-    state = {
-        tasks: []
-    };
-
-    // componentDidMount() {
-    //     fetch('http://nathan.interview.nebullam.com:1880/tasks')
-    //         .then(res => res.json())
-    //         .then((data) => {
-    //             this.setState({ tasks: data })
-    //         })
-    //         .catch(console.log)
-    // }
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/user_tasks/0">
+            <Tasks user_id='0'/>
+          </Route>
+          <Route path="/user_tasks/1">
+            <Tasks user_id='1'/>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function ActionItems() {
+  return <Tasks/>;
+}
+
+function Users() {
+  return <Tasks/>;
+}
+
+
+// import React, {Component} from 'react';
+// import Tasks from './components/tasks';
+// import {Router, Route} from 'react-router';
+// import { createBrowserHistory } from 'history';
+// // import NewTaskForm from './components/newTaskForm'
+//
+// class App extends Component {
+//
+//     render() {
+//       return(
+//         <Router history={createBrowserHistory}>
+//           <Route path="home"/> component={Tasks}
+//         </Router>
+//       )
+//         // return (
+//         //   <div>
+//         //     <h1> ACTION LIST </h1>
+//         //     <Tasks tasks={this.state.tasks} />
+//         //   </div>
+//         // )
+//     }
+//
+//     state = {
+//         tasks: []
+//     };
+// }
+//
+// export default App;
