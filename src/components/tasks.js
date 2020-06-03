a49ab1539338aa63d88dae710a53bcd2ecae762e
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-06-03 04:06:33 -0600

    refactor

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -6,12 +6,15 @@ import CheckBox from './checkBox';
 
 import {AddTaskModal} from './addTaskModal';
 import {EditTaskModal} from './editTaskModal';
+import {ProperPunct} from './properPunct';
+import '../App.css';
+
 
 export class Tasks extends Component {
 
   constructor(props){
     super(props);
-    this.state ={tasks:[], addModalShow : false, editModalShow: false, user_id: props.user_id}
+    this.state ={tasks:[], addModalShow : false, editModalShow: false, user_id: props.user_id, name: props.name}
   }
 
   componentDidMount(){
@@ -66,55 +69,61 @@ export class Tasks extends Component {
 
     return(
       <div>
-      <Table className="mt-4" stripped bordered hover size="sm">
-      <thread>
-        <tr>
-          <th><h3>Complete</h3></th>
-          <th><h3>Action Items</h3></th>
-          <th><h3>Details</h3></th>
-          <th><h3>Options</h3></th>
-        </tr>
+          <h1><strong>{this.props.name}'s Action List</strong></h1>
+          <h3> Total Tasks: {tasks.length}</h3>
+      <div>
+      <div className="task-table">
+        <Table className="mt-4" stripped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th><h3>Complete</h3></th>
+            <th><h3>Action Items</h3></th>
+            <th><h3>Details</h3></th>
+            <th><h3>Options</h3></th>
+          </tr>
 
-        </thread>
-        <tbody>
-          {tasks.map(task=>
-            <tr key ={task.id}>
-            <td>{task.complete}</td>
-            <td><strong>{task.name}</strong></td>
-            <td>{task.details}</td>
-            <td>
-              <ButtonToolbar>
-                <Button
-                  className="mr-2"
-                  variant="info"
-                  onClick = {()=> this.setState({editModalShow:true, id:task.id, name:task.name, details:task.details, complete: task.complete})}>
-                    Edit
-                </Button>
+          </thead>
+          <tbody>
+            {tasks.map(task=>
+              <tr key ={task.id}>
+              <td><CheckBox/></td>
+              <td><strong>{task.name}</strong></td>
+              <td>{task.details}</td>
+              <td>
+                <ButtonToolbar>
+                  <Button
+                    className="mr-2"
+                    variant="info"
+                    onClick = {()=> this.setState({editModalShow:true, id:task.id, name:task.name, details:task.details, complete: task.complete})}>
+                      Edit
+                  </Button>
 
-                <Button className="mr-2"
-                  onClick= {()=> this.deleteTask(task.id)}
-                  variant="danger">
-                    Delete
-                </Button>
+                  <Button className="mr-2"
+                    onClick= {()=> this.deleteTask(task.id)}
+                    variant="danger">
+                      Delete
+                  </Button>
 
-                <EditTaskModal
-                show={this.state.editModalShow}
-                onHide={editModalClose}
-                id = {this.state.id}
-                name = {this.state.name}
-                details = {this.state.details}
-                user_id = {this.state.user_id}
-                />
+                  <EditTaskModal
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                  id = {this.state.id}
+                  name = {this.state.name}
+                  details = {this.state.details}
+                  user_id = {this.state.user_id}
+                  />
 
-              </ButtonToolbar>
-            </td>
-            </tr>
-          )}
-        </tbody>
+                </ButtonToolbar>
+              </td>
+              </tr>
+            )}
+          </tbody>
 
-        </Table>
-        <ButtonToolbar>
-          <Button variant='primary' onClick={()=> this.setState({addModalShow: true})}>
+          </Table>
+        </div>
+
+        <ButtonToolbar className="btn-toolbar">
+          <Button className="add-btn" variant='primary' onClick={()=> this.setState({addModalShow: true})}>
             Add Task
           </Button>
 
@@ -122,6 +131,7 @@ export class Tasks extends Component {
 
         </ButtonToolbar>
       </div>
+    </div>
     )
 
   }
