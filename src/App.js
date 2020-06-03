90788ba2ef57a42b66d7d31843d6ac0099bd8a5d
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-06-03 04:08:09 -0600

    Add home button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import{ Dropdown, DropdownButton, Button} from 'react-bootstrap'
+import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,78 +11,60 @@ import Tasks from './components/tasks';
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/user_tasks/0">Clayton</Link>
-            </li>
-            <li>
-              <Link to="/user_tasks/1">Josh</Link>
-            </li>
-          </ul>
-        </nav>
+  <div class="dd-button">
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/user_tasks/0">
-            <Tasks user_id='0'/>
-          </Route>
-          <Route path="/user_tasks/1">
-            <Tasks user_id='1'/>
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+    <Router>
+      <div className="home-btn">
+        <Button variant="outline-primary"><Link to="/">Home</Link></Button>
       </div>
-    </Router>
-  );
-}
+
+
+      <Switch>
+        <Route path="/user_tasks/1">
+          <Tasks user_id='1' name='Clayton'/>
+        </Route>
+        <Route path="/user_tasks/2">
+          <Tasks user_id='2' name='Mahmoud'/>
+        </Route>
+        <Route path="/user_tasks/3">
+          <Tasks user_id='3' name='Josh'/>
+        </Route>
+        <Route path="/user_tasks/4">
+        < Tasks user_id='4' name='Nathan'/>
+        </Route>
+        <Route path="/user_tasks/5" >
+          <Tasks user_id='5' name='Guest'/>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      </Router>
+    </div>
+    );
+  }
 
 function Home() {
-  return <h2>Home</h2>;
+  return(
+    <div className="page-container">
+      <div className="home">
+        <h1 className="welcome">Nebullam Action List</h1>
+        <h4 className="motto">"Lettuce Make It Happen!"</h4>
+        <div>
+          <DropdownButton id="dropdown-button-drop-right" title="Who Are You?" key="right" drop="right" variant="info">
+            <Dropdown.Item eventKey="1"> <Link to="/"></Link> </Dropdown.Item>
+            <Dropdown.Item eventKey="2"> <Link to="/user_tasks/1">Clayton</Link> </Dropdown.Item>
+            <Dropdown.Item eventKey="3"> <Link to="/user_tasks/2">Mahmoud</Link> </Dropdown.Item>
+            <Dropdown.Item eventKey="4"> <Link to="/user_tasks/3">Josh</Link> </Dropdown.Item>
+            <Dropdown.Item eventKey="5"> <Link to="/user_tasks/4">Nathan</Link> </Dropdown.Item>
+            <Dropdown.Item eventKey="6"> <Link to="/user_tasks/5">Guest User</Link> </Dropdown.Item>
+          </DropdownButton>
+        </div>
+          <div className="lettuce">
+            <img src={ require('./images/laughing_lettuce.jpeg') } />
+          </div>
+      </div>
+    </div>
+  )
 }
-
-function ActionItems() {
-  return <Tasks/>;
-}
-
-function Users() {
-  return <Tasks/>;
-}
-
-
-// import React, {Component} from 'react';
-// import Tasks from './components/tasks';
-// import {Router, Route} from 'react-router';
-// import { createBrowserHistory } from 'history';
-// // import NewTaskForm from './components/newTaskForm'
-//
-// class App extends Component {
-//
-//     render() {
-//       return(
-//         <Router history={createBrowserHistory}>
-//           <Route path="home"/> component={Tasks}
-//         </Router>
-//       )
-//         // return (
-//         //   <div>
-//         //     <h1> ACTION LIST </h1>
-//         //     <Tasks tasks={this.state.tasks} />
-//         //   </div>
-//         // )
-//     }
-//
-//     state = {
-//         tasks: []
-//     };
-// }
-//
-// export default App;
